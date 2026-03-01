fc3cd1e016f3c241511150a18d0f4afee3fb3c81
hauerattilaarpad <h.attila.arpad@gmail.com>
2026-03-01 17:42:24 +0100

    add user creation, retrieval, deletion functionality

--- backend/src/shared/user_auth_service.py
+++ backend/src/shared/user_auth_service.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import TypedDict
 
 
@@ -7,14 +8,25 @@ class User(TypedDict):
 
 users:list[User] = []
 
+def generate_unique_id() -> str:
+    return str(uuid.uuid4())
+
 def add_user(user_name, user_id) -> User:
-    pass
+    new_user: User = {"id": user_id, "name": user_name}
+    users.append(new_user)
+    return new_user
 
 def remove_user(user_id) -> bool:
-    pass
+    global users
+    initial_length = len(users)
+    users = [u for u in users if u["id"] != user_id]
+    return len(users) < initial_length
 
-def get_user(user_id) -> User:
-    pass
+def get_user(user_id) -> User|None:
+    for user in users:
+        if user["id"] == user_id:
+            return user
+    return None
 
 def set_user_to_connection(data, client):
     pass
--- backend/src/shared/users_controller.py
+++ backend/src/shared/users_controller.py
@@ -1,7 +1,47 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+
+from backend.src.shared.user_auth_service import generate_unique_id, add_user, get_user, remove_user
 
 users_controller = Blueprint('users_controller', __name__)
 
-@users_controller.route('/api/users', methods=['PUT'])
+def create_response(data=None, error=None, status=200):
+    return jsonify({
+        "succes": error is None,
+        "data": data,
+        "error": error
+    }),status
+
+@users_controller.route('/api/users/<user_id>', methods=['GET'])
+def get_user_by_id(user_id):
+    user = get_user(user_id)
+
+    if not user:
+        return create_response(error="User not found", status=404)
+
+    return create_response(data=user)
+
+@users_controller.route('/api/users', methods=['POST'])
 def create_user():
-    pass
+    body = request.get_json()
+
+    if not body or 'name' not in body:
+        return create_response(error="'name' field required!", status=400)
+
+    user_name = str(body['name']).strip()
+    if not user_name:
+        return create_response(error="Name cannot be empty!", status=400)
+
+    new_id = generate_unique_id()
+    new_user = add_user(user_name, new_id)
+
+    return create_response(data=new_user, status=201)
+
+
+@users_controller.route('/api/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    is_deleted = remove_user(user_id)
+
+    if not is_deleted:
+        return create_response(error="User not found.", status=404)
+
+    return create_response(data={"message": "User successfully deleted."})
