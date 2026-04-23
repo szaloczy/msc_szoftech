3b2bd58b29836781594677dd80f6aec6636d28b6
szaloczy <szaloczy6@gmail.com>
2026-04-23 21:30:22 +0200

    extend hands and card component

--- frontend/src/app/spicy/other-player-hand/other-player-hand.ts
+++ frontend/src/app/spicy/other-player-hand/other-player-hand.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-other-player-hand',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './other-player-hand.html',
   styleUrl: './other-player-hand.css',
 })
 export class OtherPlayerHand {
+    @Input() name = '';
+    @Input() cards = 0;
 
+    getRotation(index: number): number {
+      const total = Math.min(this.cards,5);
+      const spread = 30; // degrees spread across the fan
+      return -spread / 2 + (spread / (total - 1)) * index;
+  }
 }
--- frontend/src/app/spicy/user-cards/user-cards.ts
+++ frontend/src/app/spicy/user-cards/user-cards.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CardSuit, PlaceCardEvent, SpicyCard } from '../../types_spicy';
+import { Card } from "../card/card";
 
 @Component({
   selector: 'app-user-cards',
-  imports: [],
+  imports: [Card],
   templateUrl: './user-cards.html',
   styleUrl: './user-cards.css',
 })
 export class UserCards {
+  @Input() defaultDeclaredSuit:CardSuit = CardSuit.Pepper;
+  @Input() defaultDeclaredValue = 1;
+  @Input() disabled = false;
 
+  @Input() cards: SpicyCard[] = [];
+
+  selectedCardIndex: number | null = null;
+  selectedDeclaredSuit: CardSuit = this.defaultDeclaredSuit;
+  selectedDeclaredValue: number = this.defaultDeclaredValue;
+
+  onCardSelect(index: number) {
+    this.selectedCardIndex = index;
+  }
+
+  onPlaceCard(event: PlaceCardEvent) {
+    // Should be replaced with actual logic via websocket to handle the card placement
+    this.cards.splice(event.index, 1);
+
+    if (this.selectedCardIndex === event.index) {
+      this.selectedCardIndex = null;
+    }
+  }
+
+  onDeclaredChange(event: { suit: string; value: number }) {
+    this.selectedDeclaredSuit = event.suit as CardSuit;
+    this.selectedDeclaredValue = event.value;
+  }
 }
